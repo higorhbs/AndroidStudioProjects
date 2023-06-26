a1bedabcbcc82b895cdb655fdd93a36b871fa077
Higor Henrique <101432956+higorhbs@users.noreply.github.com>
2023-06-26 20:53:10 -0300

    nome dos jogadores

--- pingpong/lib/main.dart
+++ pingpong/lib/main.dart
@@ -36,25 +36,25 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-class Atracao {
+class Player {
   final String nome;
   final int dia;
   final List<String> tags;
 
-  const Atracao(this.nome, this.dia, this.tags);
+  const Player(this.nome, this.dia, this.tags);
 }
 
 const listaAtracoes = [
-  Atracao("Iron Maiden", 2, ["Espetaculo", "Fas", "NovoAlbum"]),
-  Atracao("Alok", 3, ["Influente", "Top", "Show"]),
-  Atracao("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
-  Atracao("Guns N’ Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
-  Atracao("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
-  Atracao("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
-  Atracao("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
-  Atracao("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
-  Atracao("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
-  Atracao("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
-  Atracao("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
-  Atracao("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
+  Player("Iron Maiden", 2, ["Espetaculo", "Fas", "NovoAlbum"]),
+  Player("Alok", 3, ["Influente", "Top", "Show"]),
+  Player("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
+  Player("Guns N’ Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
+  Player("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
+  Player("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
+  Player("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
+  Player("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
+  Player("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
+  Player("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
+  Player("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
+  Player("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
 ];
