b550cfcc1eb88e254750e77001a6f0c0fa0d5fd5
Higor Henrique <101432956+higorhbs@users.noreply.github.com>
2023-06-27 20:34:18 -0300

    ajustes

--- pingpong/lib/main.dart
+++ pingpong/lib/main.dart
@@ -1,54 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const myapp());
-}
+void main() => runApp(const PerguntaApp());
 
-// ignore: camel_case_types
-class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ping Pong',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-     final jogadorEscolhido = 0;
-     void escolhaJogador(){
-  }
-
-@override
-  Widget build(BuildContext context) {
-    final List<String> jogadores = [
-      'Escolha um jogador'
+    final perguntas = [
+      'Escolha seu usuário',
+      'Qual é o seu animal favorito?',
     ];
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Jogadores'),
-      ),     
-      body: Column(
-        children: <Widget>[
-          Text(jogadores.elementAt(0)),
-        ElevatedButton(onPressed: escolhaJogador, child: const Text('Higor')),
-        ElevatedButton(onPressed: escolhaJogador, child: const Text('Victor')),
-        ElevatedButton(onPressed: escolhaJogador, child: Text('Laura')),
-        ElevatedButton(onPressed: escolhaJogador, child: Text('Outro')),
-        ],
-      )
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Escolha o Jogador'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Higor'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Victor'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Laura'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('outro'),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
